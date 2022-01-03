ee668c93bae63f44507f64c8c560ce6e1141355d
tianjiang <2116183424@qq.com>
2022-01-03 11:47:43 +0800

    登录

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
+    <login-register />
   </div>
 </template>
-
+<script>
+import LoginRegister from "./views/LoginRegister.vue";
+export default {
+  components: {
+    LoginRegister,
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +19,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/views/LoginRegister.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="sb">
+    <div>
+      <img src="../assets/logo.jpg" />
+      <button>手机号登录</button>
+      <button>注册</button>
+    </div>
+    <div>
+      <van-divider>其他登录方式</van-divider>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#sb {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+  img {
+    width: 20vw;
+    margin: 10vw auto;
+  }
+  button {
+    display: inline-block;
+    width: 90vw;
+    margin: 1vh auto;
+    height: 5vh;
+    border: 1px solid red;
+    border-radius: 25px;
+    background-color: #fff;
+    color: red;
+  }
+  div:nth-of-type(2){
+    width: 50vw;
+    font-size: 12px;
+  }
+}
+</style>
+>
